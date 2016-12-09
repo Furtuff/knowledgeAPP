0119789d48dc1cbbdc1fc22fbfdd0987d9d5e4c9
tuffery <furtuff@gmail.com>
2016-12-09 19:12:10 +0100

    calendar listview in progress

---
+++ app/src/main/java/fr/wildcodeschool/eateam/knowledgeishere/ListCalendarActivity.java
@@ -0,0 +1,18 @@
+package fr.wildcodeschool.eateam.knowledgeishere;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class ListCalendarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_calendar);
+
+        ListView firstListview = (ListView)findViewById(R.id.ListviewCalendar);
+        CustomAdapter customAdapter = new CustomAdapter(this,ArrayStatic.fakelist());
+        firstListview.setAdapter(customAdapter);
+    }
+}
