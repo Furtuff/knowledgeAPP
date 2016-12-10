56289493c552bc81d729fe4acafd77329f438151
tuffery <furtuff@gmail.com>
2016-12-10 03:04:35 +0100

    seekbar responsive

--- app/src/main/java/fr/wildcodeschool/eateam/knowledgeishere/SetTargetActivity.java
+++ app/src/main/java/fr/wildcodeschool/eateam/knowledgeishere/SetTargetActivity.java
@@ -2,6 +2,8 @@ package fr.wildcodeschool.eateam.knowledgeishere;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class SetTargetActivity extends AppCompatActivity {
 
@@ -9,5 +11,80 @@ public class SetTargetActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_target);
+        // première ligne
+        final TextView textView1 = (TextView)findViewById(R.id.textView9);
+        SeekBar seekBar1 = (SeekBar)findViewById(R.id.seekBar1);
+        seekBar1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                textView1.setText(Integer.toString(i));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        //deuxième
+        final TextView textView2 = (TextView)findViewById(R.id.textView10);
+        SeekBar seekBar2 = (SeekBar)findViewById(R.id.seekBar2);
+        seekBar2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                textView2.setText(Integer.toString(i));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        //troisième
+        final TextView textView3 = (TextView)findViewById(R.id.textView11);
+        SeekBar seekBar3 = (SeekBar)findViewById(R.id.seekBar3);
+        seekBar3.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                textView3.setText(Integer.toString(i));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+        final TextView textView4 = (TextView)findViewById(R.id.textView12);
+        SeekBar seekBar4 = (SeekBar)findViewById(R.id.seekBar4);
+        seekBar4.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                textView4.setText(Integer.toString(i));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
