d9f05bf6fdffa0db8c09398ce8598f5337162a05
tuffery <furtuff@gmail.com>
2016-12-11 13:46:32 +0100

    dashboard responsive and more

--- app/src/main/java/fr/wildcodeschool/eateam/knowledgeishere/LastTabActivity/DashBoardActivity.java
+++ app/src/main/java/fr/wildcodeschool/eateam/knowledgeishere/LastTabActivity/DashBoardActivity.java
@@ -17,7 +17,7 @@ public class DashBoardActivity extends AppCompatActivity implements Bidouille  {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dash_board);
-        this.setTitle("DasBoard");
+        this.setTitle("DasHBoard");
 
 
         // Customize SpeedometerGauge
