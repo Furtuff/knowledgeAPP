cb47c858fdebb65a18ca96ff8d2555c97d980c84
tuffery <furtuff@gmail.com>
2016-12-12 09:59:18 +0100

    theme Fix

--- app/src/main/java/fr/wildcodeschool/eateam/knowledgeishere/LastTabActivity/LastTabActivity.java
+++ app/src/main/java/fr/wildcodeschool/eateam/knowledgeishere/LastTabActivity/LastTabActivity.java
@@ -3,9 +3,13 @@ package fr.wildcodeschool.eateam.knowledgeishere.LastTabActivity;
 import android.app.TabActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.LinearLayout;
 import android.widget.TabHost;
+import android.widget.TextView;
 
 import fr.wildcodeschool.eateam.knowledgeishere.R;
+
+import static android.R.attr.width;
 
 
 public class LastTabActivity extends TabActivity {
@@ -24,7 +28,15 @@ public class LastTabActivity extends TabActivity {
         mTabHost.addTab(mTabHost.newTabSpec("second").setIndicator("Action Plan").setContent(new Intent(this , ActionPlanActivity.class )));
         mTabHost.addTab(mTabHost.newTabSpec("third").setIndicator("Dash-Board").setContent(new Intent(this , DashBoardActivity.class )));
         mTabHost.addTab(mTabHost.newTabSpec("four").setIndicator("News Feed").setContent(new Intent(this , NewsFeedActivity.class )));
+/*
         mTabHost.addTab(mTabHost.newTabSpec("five").setIndicator("...").setContent(new Intent(this , ThreePointActivity.class )));
+*/
         mTabHost.setCurrentTab(clicked);
+for (int i = 0; i<4;i++) {
+    TextView tv = (TextView) mTabHost.getTabWidget().getChildAt(i).findViewById(android.R.id.title);
+    tv.setAllCaps(false);
+/*    mTabHost.getTabWidget().getChildAt(i).setLayoutParams(new
+            LinearLayout.LayoutParams((width/5)-2,50));*/
+}
     }
 }
